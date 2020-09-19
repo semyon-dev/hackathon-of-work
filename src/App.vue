856e2b84481b9a9a371dccd46ae58c30fb995ea1
semyon-dev <semyon-dev@protonmail.com>
2020-09-19 19:03:32 +0300

    add icon

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.jpg" width="300">
+    <CreateResume></CreateResume>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CreateResume from "@/components/CreateResume";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CreateResume,
   }
 }
 </script>
