2226b8ec725dfbc289f170985063887604d10beb
3xlerman <lermanworks@gmail.com>
2020-09-19 16:09:19 +0300

    feat: resumes

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"hackathon-work/db"
+	"strings"
 )
 
 func main() {
@@ -11,14 +12,23 @@ func main() {
 
 	duties, demands := KeyWords()
 
-	fmt.Println(duties, demands)
-
 	var text = "Обязанности:   Приготовление напитков в баре  Прием и подача заказов по столикам Работа с кассой\n" +
 		"Требования:  Желание работать и зарабатывать. Опыт работы на подобной должности будет Вашим преимуществом.\n" +
 		"А вообще всему научим и всему обучим, так что не нужно бояться приходить без опыта работы). Наличие медицинской книжки будет Вашим преимуществом.\n" +
 		"Условия:  График сменный 2\\2. Будние дни с 9-00 до 21-00. Выходные дни с 9-00 до 21-00. Оформление по ТК. Оплата: 100 руб/час + % + чаевые"
 
 	_ = text
+
+	resumes := db.GetResumes(100, 0)
+	for _, resume := range resumes {
+		for _, word := range duties {
+			if strings.Contains(strings.ToLower(resume.Description), word) {
+				index := strings.Index(strings.ToLower(resume.Description), word)
+				temp := resume.Description[index:]
+
+			}
+		}
+	}
 }
 
 func KeyWords() ([]string, []string)  {
